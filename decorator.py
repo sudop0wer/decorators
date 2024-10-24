ccd9e1154d2b1f7e72b6ac57497aec31bc9b1517
sudop0wer <tugleacristian@gmail.com>
2024-10-24 14:50:44 +0300

    added login_in and log_out methods, logged_in variable that tracks the status for each user

--- decorator.py
+++ decorator.py
@@ -2,15 +2,28 @@
 class User:
 
     all = {}
+    logged_in = False
 
-    def __init__(self, user_id, user_password, logged_in=False):
+    def __init__(self, user_id, user_password):
         self.user_id = user_id
         self.user_password = user_password
         User.all.setdefault(user_id, user_password)
 
-    #def log_in(self, user_id, user_password):
-    #    if all.get(f"{user_id}"):
+    def log_in(self, user_id='', user_password=''):
+        user_id = input("Type user ID: ")
+        #if User.all.get(user_id) is None:
+        if user_id != self.user_id:
+            print("Wrong username")
+        else:
+            user_password = input("Type your password: ")
+            if User.all[user_id] == user_password:
+                print("Login successfully!")
+                self.logged_in = True
+            else:
+                print("Wrong password! Retry")
 
+    def log_out(self, user_id=''):
+        self.logged_in = False
 
 
 # def requires_authentication(func):
@@ -19,4 +32,13 @@ user1 = User('alex', 'password123')
 user2 = User('mark', 'cisco123!')
 user3 = User('radu', 'danu')
 
-print(User.all)
+
+user1.log_in()
+user1.log_out()
+
+
+#print(user1.logged_in)
+#print(f"User 1 logged in: {user1.logged_in}")
+#print(f"User 2 logged in: {user2.logged_in}")
+#print(f"User 3 logged in: {user3.logged_in}")
+#print(User.all)
